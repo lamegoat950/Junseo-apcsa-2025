db9b8a0e6245d11d5aaae6b929a4a4a8eb42d06f
lamegoat950 <yoonjoonseo0610@gmail.com>
2026-01-08 04:48:55 +0000

    finished problem number 5

--- warmup/Main.java
+++ warmup/Main.java
@@ -2,22 +2,53 @@ package warmup;
 
 public class Main {
 
-     public static void main(String[] args){ //problem number 2(answer might be hard to see but it's rihgt next to @lamegoat950)
-    int num1;
-    int num2 = 0;
-    int num3 = 1;
-    for (int i = 0; i < 14; i++) {
-            num1 = num2;
-            num2 = num3;
-            num3 = num1 + num2;
-            
-  if(num3 % 2 == 0) {
-        int plus = 0;
-        plus = plus + num3;
-        System.out.print(plus);
+public static void main(String[] args){
+    //problem number 5
+int sum = 0;
+for(int i = 1; i < 1000;i++) {
+  if((i % 3 == 0)||(i % 5 == 0)) {
+    sum += i;
+  } 
+          System.out.print(sum + " ");
   }
 }
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     //public static void main(String[] args){ //problem number 2(answer might be hard to see but it's rihgt next to @lamegoat950)
+   // int num1;
+   // int num2 = 0;
+   // int num3 = 1;
+   // for (int i = 0; i < 14; i++) {
+     //       num1 = num2;
+    //        num2 = num3;
+   //         num3 = num1 + num2;
+
+  //if(num3 % 2 == 0) {
+    //    int plus = 0;
+     //   plus = plus + num3;
+     //   System.out.print(plus);
+  // }
+// }
+// }
 
 
