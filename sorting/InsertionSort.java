e21f2df7bdc439fc877bb26706be1cc0d837dcae
lamegoat950 <yoonjoonseo0610@gmail.com>
2026-01-27 03:05:17 +0000

    finished insertion sort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,17 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int n = input.length;
+
+        for(int l = 1; l < n; l++) {
+           int temp = input[l];
+           int i = l - 1;
+
+           while(i >=0 && input[i] > temp){
+            input[i+1] = input[i];
+            i = i -1;
+           }
+           input[i+1] = temp;
+            }
+        }
     }
-}
