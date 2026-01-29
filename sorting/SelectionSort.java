dc0132bef01a951367c3505bd3ee124c239a4b45
lamegoat950 <yoonjoonseo0610@gmail.com>
2026-01-29 05:36:22 +0000

    finished selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,5 +4,19 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        int l = input.length;
+
+        for (int i = 0; i < l; i++) {
+            int minimum = i;
+
+            for(int o = i + 1; o < l; o++) {
+                if(input[o] < input[minimum]) {
+                    minimum = o;
+                }
+            }
+            int temp = input[minimum];
+            input[minimum] = input[i];
+            input[i] = temp;
+        }
     }
 }
