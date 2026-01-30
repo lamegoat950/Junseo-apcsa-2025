ce6f66fc5102fd0a96c02b8a3dfd8efc13f134c2
lamegoat950 <yoonjoonseo0610@gmail.com>
2026-01-30 16:29:19 +0000

    finished main

--- sorting/Main.java
+++ sorting/Main.java
@@ -1,5 +1,6 @@
 package sorting;
 
+
 public class Main {
     public static void main(String[] args)
     {
@@ -13,7 +14,7 @@ public class Main {
         //     of InsertionSort and Selection sort continue to work.
         //  4. Increase the loop count for TestSuite to get better performance results.
 
-        int[] testInput = randomArray(5);
+        int[] testInput = randomArray(300);
 
         // Increase the loop count to get better results once it works.
         // TestSuite.run(testInput, 10000);
@@ -23,8 +24,13 @@ public class Main {
     public static int[] randomArray(int length)
     {
         // TODO: make this return an actual random array of the provided length.
-        int[] a = {10, 5, 3, 2};
-        return a;
+        int[] j = new int[length];
+        
+
+        for(int i = 0; i < length; i++) {
+            j[i] = ((int)(Math.random()*10)+1);
+        }
+        return j;
     }
 
 }
